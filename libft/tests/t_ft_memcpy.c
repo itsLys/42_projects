de08965ca0d4e90818432610147cd4b99900b281
Iliass Hajji <ihajji@e1r8p7.1337.ma>
2024-10-28 20:03:13 +0100

    d

--- libft/tests/t_ft_memcpy.c
+++ libft/tests/t_ft_memcpy.c
@@ -52,9 +52,41 @@ void test_mem_cpy(const char *src, size_t size)
     printf("\n");
 }
 
+void test_mem_cpy_overlap(const char *src, const char *src2, size_t size, int offset)
+{
+    char *dst = (char *)(src + offset);
+    char *dstft = (char *)(src2 + offset);
+    ft_memcpy(dstft, dst, size - offset);
+    printf("---------------------------\n");
+    printf("case number: %zu\n", get_case());
+    printf("src:		");
+    print_arr(src, size);
+    printf("dst:		");
+    print_arr(dst, size - offset);
+
+    printf("ft dst:		");
+    print_arr(dstft, size - offset);
+
+    memcpy(dst, src, size - offset);
+    ft_memcpy(dstft, src, size - offset);
+
+    printf("src:		");
+    print_arr(src, size);
+
+    printf("dst copied:	");
+    print_arr(dst, size - offset);
+
+    printf("ftdst copied:	");
+    print_arr(dst, size - offset);
+    assert(memcmp(dst, dstft, size - offset) == 0);
+
+    printf("\n");
+}
+
 int main()
 {
     const char src[] = {'\x00', '\x41', '\x02', '\x01', '\xf0', '\x71', '\xff', '\x7f', '\x09', '\x1f', '\x0f'};
+    const char src2[] = {'\x00', '\x41', '\x02', '\x01', '\xf0', '\x71', '\xff', '\x7f', '\x09', '\x1f', '\x0f'};
     test_mem_cpy(src, 11);
     test_mem_cpy(src, 5);
     test_mem_cpy(src, 1);
@@ -64,10 +96,11 @@ int main()
     test_mem_cpy(src0, 5);
     test_mem_cpy(src0, 1);
     test_mem_cpy(src0, 0);
-    const char *srcnull = NULL;
-    test_mem_cpy(srcnull, 11);
+    // const char *srcnull = NULL;
+    // test_mem_cpy(srcnull, 11);
     // test_mem_cpy(srcnull, 5);
     // test_mem_cpy(srcnull, 1);
     // test_mem_cpy(srcnull, 0);
     // test_same_mem_diff_null();
+    test_mem_cpy_overlap(src, src2, 11, 3);
 }
