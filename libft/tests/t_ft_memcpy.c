201b41aa5bb0e512340cd0a348d1fce86baf689d
Iliass Hajji <ihajji@e1r11p3.1337.ma>
2024-10-28 15:24:52 +0100

    d

--- libft/tests/t_ft_memcpy.c
+++ libft/tests/t_ft_memcpy.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 
+void test_same_mem_same_size(void);
 size_t get_case()
 {
     static size_t i = 0;
@@ -22,40 +23,6 @@ void print_arr(const void *arr, size_t size)
     }
 }
 
-void test_same_mem_same_size()
-{
-    const char buff1[] = {'\x00', '\x41', '\x02', '\x01', '\xf0', '\x71', '\xff', '\x7f', '\x09', '\x1f', '\x0f'};
-    const char buff2[] = {'\x00', '\x41', '\x02', '\x01', '\xf0', '\x71', '\xff', '\x7f', '\x09', '\x1f', '\x0f'};
-    int cmp;
-    int ft_cmp;
-    printf("---------------------------\n");
-    printf("case number: %zu\n", get_case());
-    printf("array 1: ");
-    print_arr(buff1, 11);
-    printf("array 2: ");
-    print_arr(buff2, 11);
-    printf("\n");
-    cmp = memcmp(buff1, buff2, 11);
-    ft_cmp = ft_memcmp(buff1, buff2, 11);
-    printf("memcmp value: %d\n", cmp);
-    printf("memcmp interpertation: %d\n", cmp);
-    if (cmp > 0)
-        printf("buff1 > buff2\n");
-    else if (cmp < 0)
-        printf("buff1 < buff2\n");
-    else if (cmp == 0)
-        printf("buff1 =  buff2\n");
-    printf("ft_memcmp value: %d\n", ft_cmp);
-    printf("memft_cmp interpertation: %d\n", ft_cmp);
-    if (ft_cmp > 0)
-        printf("buff1 > buff2\n");
-    else if (ft_cmp < 0)
-        printf("buff1 < buff2\n");
-    else if (ft_cmp == 0)
-        printf("buff1 =  buff2\n");
-    assert((cmp > 0 && ft_cmp > 0) || (cmp < 0 && ft_cmp < 0) || (!cmp && !ft_cmp));
-    printf("\n");
-}
 
 void test_same_mem_diff_size()
 {
@@ -244,4 +211,39 @@ int main()
     test_same_mem_diff_size3();
     test_same_mem_diff_size4();
     // test_same_mem_diff_null();
+}
+
+void test_same_mem_same_size()
+{
+    const char buff1[] = {'\x00', '\x41', '\x02', '\x01', '\xf0', '\x71', '\xff', '\x7f', '\x09', '\x1f', '\x0f'};
+    const char buff2[] = {'\x00', '\x41', '\x02', '\x01', '\xf0', '\x71', '\xff', '\x7f', '\x09', '\x1f', '\x0f'};
+    int cmp;
+    int ft_cmp;
+    printf("---------------------------\n");
+    printf("case number: %zu\n", get_case());
+    printf("array 1: ");
+    print_arr(buff1, 11);
+    printf("array 2: ");
+    print_arr(buff2, 11);
+    printf("\n");
+    cmp = memcmp(buff1, buff2, 11);
+    ft_cmp = ft_memcmp(buff1, buff2, 11);
+    printf("memcmp value: %d\n", cmp);
+    printf("memcmp interpertation: %d\n", cmp);
+    if (cmp > 0)
+        printf("buff1 > buff2\n");
+    else if (cmp < 0)
+        printf("buff1 < buff2\n");
+    else if (cmp == 0)
+        printf("buff1 =  buff2\n");
+    printf("ft_memcmp value: %d\n", ft_cmp);
+    printf("memft_cmp interpertation: %d\n", ft_cmp);
+    if (ft_cmp > 0)
+        printf("buff1 > buff2\n");
+    else if (ft_cmp < 0)
+        printf("buff1 < buff2\n");
+    else if (ft_cmp == 0)
+        printf("buff1 =  buff2\n");
+    assert((cmp > 0 && ft_cmp > 0) || (cmp < 0 && ft_cmp < 0) || (!cmp && !ft_cmp));
+    printf("\n");
 }
