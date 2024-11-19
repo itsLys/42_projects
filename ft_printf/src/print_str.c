7853aed3226bc96b9724118071ce9166d8c115c9
itsLys <elyesseen@gmail.com>
2024-11-19 17:36:05 +0100

    's'

--- ft_printf/src/print_str.c
+++ ft_printf/src/print_str.c
@@ -11,7 +11,7 @@ int putnstr(char *str, int n)
 int print_str(char *str, t_flags *f)
 {
 	int count = 0;
-	size_t strlen = ft_strlen(str);
+	int strlen = ft_strlen(str);
 
 	if (f->precision_flag)
 	{
@@ -25,3 +25,4 @@ int print_str(char *str, t_flags *f)
 	}
 	return count;
 }
+// TODO: combination of width and precision
--- ft_printf/str_examples.c
+++ ft_printf/str_examples.c
@@ -6,5 +6,24 @@ int main(void)
 	printf("String examples: \n");
 	printf("Default:	|%s|	|	Flag: '.3':	|%.3s|\n", str, str);
 	printf("Default:	|%s|	|	Flag: '.0':	|%.0s|\n", str, str);
-	printf("Default:	|%s|	|	Flag: '.':	|%.0s|\n", str, str);
+	printf("Default:	|%s|	|	Flag: '.':	|%.s|\n", str, str);
+	printf("Default:	|%s|	|	Flag: '.10':	|%.10s|\n", str, str);
+
+	printf("Default:	|%s|	|	Flag: '3':	|%3s|\n", str, str);
+	printf("Default:	|%s|	|	Flag: '10':	|%10s|\n", str, str);
+
+	printf("Default:	|%s|	|	Flag: '-3':	|%-3s|\n", str, str);
+	printf("Default:	|%s|	|	Flag: '-0':	|%-0s|\n", str, str);
+	printf("Default:	|%s|	|	Flag: '-':	|%-s|\n", str, str);
+	printf("Default:	|%s|	|	Flag: '-10':	|%-10s|\n", str, str);
+
+	printf("Default:	|%s|	|	Flag: '-.3':	|%-3s|\n", str, str);
+	printf("Default:	|%s|	|	Flag: '-.0':	|%-0s|\n", str, str);
+	printf("Default:	|%s|	|	Flag: '-.':	|%-s|\n", str, str);
+	printf("Default:	|%s|	|	Flag: '-.10':	|%-10s|\n", str, str);
+
+	printf("Default:	|%s|	|	Flag: '-5.3':	|%-3s|\n", str, str);
+	printf("Default:	|%s|	|	Flag: '-5.0':	|%-0s|\n", str, str);
+	printf("Default:	|%s|	|	Flag: '-5.':	|%-s|\n", str, str);
+	printf("Default:	|%s|	|	Flag: '-5.10':	|%-10s|\n", str, str);
 }
