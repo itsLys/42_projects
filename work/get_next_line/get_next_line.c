69ea02f4f48dbf867aac54952e36b55ffe13945e
itsLys <elyesseen@gmail.com>
2024-12-04 14:04:20 +0100

    scratch that

--- work/get_next_line/get_next_line.c
+++ work/get_next_line/get_next_line.c
@@ -6,36 +6,16 @@
 
 char	*read_line(int fd, char *last)
 {
-	char	*line;
-	ssize_t	bytes_read;
+	char *buffer = malloc(BUFFER_SIZE);
 
-	last = malloc(BUFFER_SIZE + 1);
-	if (!last)
-		return NULL;
-	while (ft_strchr(last, NL) == NULL)
-	{
-		bytes_read = read(fd, last, BUFFER_SIZE);
-		if (bytes_read == -1)
-		{
-			free(last);
-			return NULL;
-		}
-		printf("last:::::	%s\n", last);
-		last[bytes_read] = '\0';
-		line = ft_strjoin(line, last);
-	}
-	return line;
+	ssize_t bytes = read(fd, last, BUFFER_SIZE);
+
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*last;
-	char *line;
-	printf("LAST:::::%s\n", last);
-
-	if (fd < 0 || fd > MAX_FD|| BUFFER_SIZE <= 0)
-		return (NULL);
-	line = read_line(fd, last);
-	return (line);
+	read_line(fd, last);
+	printf("last->	|%s|\n", last);
 }
 // before reading a line, check the last read buffer if it has lines that should be returned
