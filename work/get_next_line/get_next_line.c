25b9b6df77d7345575656a2335802b0cbc85e34d
itsLys <elyesseen@gmail.com>
2024-12-08 12:53:34 +0100

    g

--- work/get_next_line/get_next_line.c
+++ work/get_next_line/get_next_line.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
 static char	*extract_line(char **last)
 {
--- work/get_next_line/main.c
+++ work/get_next_line/main.c
@@ -4,29 +4,67 @@
 #include <string.h>
 #include <unistd.h>
 
+void	print_gnl(char *line)
+{
+	char *str;
+
+	if (!line)
+		str = "(NULL)\n";
+	else
+		str = line;
+	printf("line->		%s", str);
+	free(line);
+}
+
+void t(void)
+{
+	printf("----------------------------------------\n");
+	int fd = open("f14.txt", O_RDONLY);
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+	print_gnl(get_next_line(fd));
+}
+
 int main(void)
 {
 	printf("BUFFER_SIZE->		%lx\n", (long unsigned) BUFFER_SIZE);
 	printf("sizeof BUFFER_SIZE->	%lu\n", sizeof(BUFFER_SIZE));
 
-	int fd = open("file2.txt", O_RDWR);
-	write(fd, "Sorem ipsum dolor sit amet, consectetur adipiscing elit.\nSed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nThis is the last line without a newline", strlen("Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nSed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nThis is the last line without a newline"));
-	lseek(fd, 0, SEEK_SET);
-	char *line;
-	printf("str->	%s", (line = get_next_line(fd)) != NULL ? line : "(null)\n");
-	printf("str->	%s", (line = get_next_line(fd)) != NULL ? line : "(null)\n");
-	printf("str->	%s", (line = get_next_line(fd)) != NULL ? line : "(null)\n");
-	printf("str->	%s", (line = get_next_line(fd)) != NULL ? line : "(null)\n");
-	printf("str->	%s", (line = get_next_line(fd)) != NULL ? line : "(null)\n");
-	printf("str->	%s", (line = get_next_line(fd)) != NULL ? line : "(null)\n");
-	printf("str->	%s", (line = get_next_line(fd)) != NULL ? line : "(null)\n");
-	printf("str->	%s", (line = get_next_line(fd)) != NULL ? line : "(null)\n");
-	printf("str->	%s", (line = get_next_line(fd)) != NULL ? line : "(null)\n");
-	// get_next_line(fd);
-	// get_next_line(fd);
-	// get_next_line(fd);
-	// get_next_line(fd);
-	// get_next_line(fd);
-	// get_next_line(fd);
-	// get_next_line(fd);
+	t();
 }
